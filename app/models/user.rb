6d32ea09c3ffb617aef2cadf0e84d7f5504c59bb
Hugo Araujo de Sousa <ha2398@columbia.edu>
2015-09-20 18:54:14 -0400

    Homework 2 - Step 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User
-	include Mongoid::Document
-
-	field :first_name, :type => String
-	field :last_name, :type => String
-	field :emall, :type => String
+  include Mongoid::Document
+  field :first_name, type: String
+  field :last_name, type: String
+  field :email, type: String
 end
