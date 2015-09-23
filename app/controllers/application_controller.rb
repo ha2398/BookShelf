a906788d7add7637a775e018cd8c071643d18778
Hugo Araujo de Sousa <ha2398@columbia.edu>
2015-09-22 23:17:54 -0400

    Homework 2 - Sign Up Working - Log In Broken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  # before_filter :require_user
+	
+ before_filter :require_user
+ helper_method :current_user
 
 
   def current_user
     if @current_user.nil?
       @current_user = User.find(session[:user_id]) if session[:user]
     end
-  end
-  helper_method :current_user
+  end  
   
   def require_user
     if current_user
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'sessions#new'
+  # root 'sessions#new'
+
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
